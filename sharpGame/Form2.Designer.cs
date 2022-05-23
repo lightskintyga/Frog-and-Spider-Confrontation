91919074903df65156d1654227759d23a338584a
lightskintyga <92919015+lightskintyga@users.noreply.github.com>
2022-05-23 11:47:27 +0500

    Update Form2.Designer.cs

--- sharpGame/Form2.Designer.cs
+++ sharpGame/Form2.Designer.cs
@@ -201,5 +201,6 @@ namespace sharpGame
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
