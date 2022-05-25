3eec837889b84065d01729ac37b6b3b806a987b2
lightskintyga <92919015+lightskintyga@users.noreply.github.com>
2022-05-25 11:30:02 +0500

    Add files via upload

---
+++ sharpGame/Fly.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace sharpGame
+{
+    class Fly
+    {
+        public static void MaximumHeight(Control x)
+        {
+            x.Top -= 3;
+            if (x.Top < 200)
+                x.Top = 200;
+        }
+    }
+}
